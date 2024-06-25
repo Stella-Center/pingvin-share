a5c4b8577279a474d813604817e0526fc86f7246
Maksym98d <maks.diakun@stellacenter.com>
2024-06-25 13:48:43 -0400

    mixpanel key change name

--- frontend/src/utils/mixpanel.util.ts
+++ frontend/src/utils/mixpanel.util.ts
@@ -1,6 +1,7 @@
 import mixpanel from "mixpanel-browser";
 export const initMixPanel = () => {
   if (typeof window !== "undefined") {
+    console.log("v", process.env.NEXT_PUBLIC_MIXPANEL_KEY);
     mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_KEY || "");
   }
 };
